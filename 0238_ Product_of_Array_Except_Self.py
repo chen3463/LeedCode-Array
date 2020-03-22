687663b78994da02b76b1eb9bdfb53a4dde17a12
chen3463 <cqm0802@hotmail.com>
2020-03-22 16:21:29 -0500

    Update 0238_ Product_of_Array_Except_Self.py

--- 0238_ Product_of_Array_Except_Self.py
+++ 0238_ Product_of_Array_Except_Self.py
@@ -18,9 +18,10 @@ class Solution:
         res = [0] * len(nums)
         left = [1] * len(nums)
         right = [1] * len(nums)
+        
         for i in range(1, len(nums)): 
-            # res += [self.multiply(nums[0:i]) * self.multiply(nums[(i+1):])]
             left[i] = nums[i-1] * left[i-1]
+            
         for j in range(len(nums)-2, -1, -1):
             right[j] = nums[j+1] * right[j+1] 
             
